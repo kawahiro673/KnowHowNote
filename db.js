e0f686ae996b6eaa98b7594a315cb68b4742c796
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-05 00:21:46 +0900

    MySQL 接続確認

---
+++ db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql'); //mysqlを使うためのやーつ
+
+const exports = mysql.createConnection({
+  host: 'us-cdbr-east-06.cleardb.net',
+  port: 3306,
+  user: 'b7a48a6bf21f12',
+  password: '386777a7',
+  database: 'heroku_436d62cc5e9f7c4',
+});
+
+module.exports = exports;
