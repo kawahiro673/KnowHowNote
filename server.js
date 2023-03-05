a590217707725565c91f8fb121760247c1c385cf
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-05 22:11:14 +0900

    mysql change

--- server.js
+++ server.js
@@ -183,6 +183,7 @@ app
         connection.query(
           'SELECT * FROM tab_hold ORDER BY tabOrder;',
           (error, results) => {
+            console.log(results);
             res.send({ response: results });
           }
         );
