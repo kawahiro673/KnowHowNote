c286ac8234ad8368308a788e43e834762d91c383
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-05 15:35:53 +0900

    mysql change

--- server.js
+++ server.js
@@ -114,6 +114,9 @@ app
                     connection.query(
                       'select * from folder order by folder_order ASC',
                       (error, result_folder) => {
+                        console.log('*****************');
+                        console.log(process.env.DATABASE_URL);
+                        console.log('*****************');
                         // 上のクエリ文が result に入る
                         res.render('index.ejs', {
                           old_memo: result,
