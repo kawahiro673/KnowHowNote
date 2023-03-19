c1e7de5cd130b6cbbbfee369451da489169241a8
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-19 21:51:16 +0900

    login proccess

--- routes/srv_login.js
+++ routes/srv_login.js
@@ -10,7 +10,7 @@ router
   })
   .post(async (req, res) => {
     if (req.body.flg === 'info') {
-      pool.query('SELECT * FROM register_user;', (error, result) => {
+      pool.query('SELECT * FROM register_user;', async (error, result) => {
         const user = result.find((user) => user.Email === req.body.email);
         if (!user) {
           return res.status(400).json([
@@ -18,29 +18,32 @@ router
               message: 'そのユーザは存在しません',
             },
           ]);
-        }    
+        }
         //パスワードの復号・照合(true or falseを返す)
-        const isMatch = await bcrypt.compare(req.body.password,result[0].HashedPassword);
-        if(!isMatch){
-            return res.status(400).json([
-                {
-                  message: 'パスワードが異なります',
-                },
-              ]);
+        const isMatch = await bcrypt.compare(
+          req.body.password,
+          result[0].HashedPassword
+        );
+        if (!isMatch) {
+          return res.status(400).json([
+            {
+              message: 'パスワードが異なります',
+            },
+          ]);
         }
 
-         //クライアントへJWTの発行(クライアント側のトークンはローカルストレージに保存するのはだめ。Cookieを使って保存する。)
-      const token = await JWT.sign(
-        {
-          email,
-        },
-        'SECRET_KEY', //秘密鍵。envファイルとかに隠す。
-        {
-          expiresIn: '24h',
-        }
-      );
-      //ここでクラアントに返した値(token)をCookieに保存させる
-      res.send({ token: token });
+        //クライアントへJWTの発行(クライアント側のトークンはローカルストレージに保存するのはだめ。Cookieを使って保存する。)
+        const token = await JWT.sign(
+          {
+            email,
+          },
+          'SECRET_KEY', //秘密鍵。envファイルとかに隠す。
+          {
+            expiresIn: '24h',
+          }
+        );
+        //ここでクラアントに返した値(token)をCookieに保存させる
+        res.send({ token: token });
       });
     }
   });
