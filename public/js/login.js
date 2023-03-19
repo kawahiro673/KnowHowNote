2dcb191b06992fa6c350db53b3fdc6e210eae957
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-19 16:11:04 +0900

    login proccess

--- public/js/login.js
+++ public/js/login.js
@@ -36,19 +36,23 @@ function registerButtonClick() {
       );
       if (user) {
         alert('そのユーザーは登録できません');
+        userName.style.backgroundColor = '#FFBEDA';
         return false;
       }
       //emailバリデーションチェック
       if (!email.value.match(/.+@.+\..+/)) {
         alert('正しいメールアドレスを入力してください');
+        email.style.backgroundColor = '#FFBEDA';
         return false;
       }
       //パスワード２回目入力チェック
       if (password.value !== cfPassword.value) {
         alert('パスワードの入力に誤りがあります');
+        password.style.backgroundColor = '#FFBEDA';
+        cfPassword.style.backgroundColor = '#FFBEDA';
         return false;
       }
-
+      //既に登録されているemailがあれば「登録済み」とする
       const mail = res.response.find((user) => user.Email === email.value);
       if (mail) {
         alert('既にそのユーザは存在しています');
