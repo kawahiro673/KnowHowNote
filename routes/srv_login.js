591c75cd5974a67b9d20597db35a03e78978e161
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-19 22:02:25 +0900

    login proccess

--- routes/srv_login.js
+++ routes/srv_login.js
@@ -13,11 +13,7 @@ router
       pool.query('SELECT * FROM register_user;', async (error, result) => {
         const user = result.find((user) => user.Email === req.body.email);
         if (!user) {
-          return res.status(400).json([
-            {
-              message: 'そのユーザは存在しません',
-            },
-          ]);
+          res.send({ message: 'そのユーザは存在しません' });
         }
         //パスワードの復号・照合(true or falseを返す)
         const isMatch = await bcrypt.compare(
@@ -25,11 +21,7 @@ router
           result[0].HashedPassword
         );
         if (!isMatch) {
-          return res.status(400).json([
-            {
-              message: 'パスワードが異なります',
-            },
-          ]);
+          res.send({ message: 'パスワードが異なります' });
         }
 
         //クライアントへJWTの発行(クライアント側のトークンはローカルストレージに保存するのはだめ。Cookieを使って保存する。)
