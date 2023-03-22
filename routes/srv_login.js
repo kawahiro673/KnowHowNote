bc05ac02efbe21ae2e028a025c6c71cca82c097d
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-22 23:04:33 +0900

    login proccess

--- routes/srv_login.js
+++ routes/srv_login.js
@@ -50,8 +50,8 @@ router
 
         res.cookie('token', token, options);
 
-        console.log(token);
-        return res.redirect('/mypage/');
+        //return res.redirect('/mypage/');
+        res.render('index.ejs');
       });
     }
   });
