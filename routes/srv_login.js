336bf965ba77bafab337f09c92c394b8a9dec867
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-28 23:13:25 +0900

    login proccess

--- routes/srv_login.js
+++ routes/srv_login.js
@@ -4,57 +4,57 @@ const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
 const check = require('./check');
 
-router
-  .route('/')
-  .get((req, res) => {
-    res.render('login.ejs');
-  })
-  .post(async (req, res) => {
-    let email = req.body.email;
-    if (req.body.flg === 'info') {
-      pool.query('SELECT * FROM register_user;', async (error, result) => {
-        const user = result.find((user) => user.Email === req.body.email);
-        //console.log(user);
-        if (!user) {
-          res.send({
-            message: 'メールアドレスまたはパスワードが間違っています',
-          });
+//Getでページを表示した際にcheckでCookieに保存されているトークンを確認し、自動ログインするか否か判断
+router.get('/', check, (req, res) => {
+  res.render('login.ejs');
+});
+
+router.post('/', async (req, res) => {
+  let email = req.body.email;
+  if (req.body.flg === 'info') {
+    pool.query('SELECT * FROM register_user;', async (error, result) => {
+      const user = result.find((user) => user.Email === req.body.email);
+      //console.log(user);
+      if (!user) {
+        res.send({
+          message: 'メールアドレスまたはパスワードが間違っています',
+        });
+      }
+      //パスワードの復号・照合(true or falseを返す)
+      const isMatch = await bcrypt.compare(
+        req.body.password,
+        user.HashedPassword
+      );
+      if (!isMatch) {
+        res.send({
+          message: 'メールアドレスまたはパスワードが間違っています',
+        });
+      }
+
+      //クライアントへJWTの発行(クライアント側のトークンはローカルストレージに保存するのはだめ。Cookieを使って保存する。)
+      const token = await JWT.sign(
+        {
+          email,
+        },
+        'SECRET_KEY', //秘密鍵。envファイルとかに隠す。
+        {
+          expiresIn: '24h',
         }
-        //パスワードの復号・照合(true or falseを返す)
-        const isMatch = await bcrypt.compare(
-          req.body.password,
-          user.HashedPassword
-        );
-        if (!isMatch) {
-          res.send({
-            message: 'メールアドレスまたはパスワードが間違っています',
-          });
-        }
+      );
 
-        //クライアントへJWTの発行(クライアント側のトークンはローカルストレージに保存するのはだめ。Cookieを使って保存する。)
-        const token = await JWT.sign(
-          {
-            email,
-          },
-          'SECRET_KEY', //秘密鍵。envファイルとかに隠す。
-          {
-            expiresIn: '24h',
-          }
-        );
+      const options = {
+        //httpOnly: true, // JavaScriptからアクセスできないようにする(document.cookieで取得もできない)
+        maxAge: 1000 * 60 * 60, // 有効期限を設定(ミリ秒)→アプリケーション>Cookieに保存されているの確認
+        // secure: process.env.NODE_ENV === 'production', // HTTPS上でのみ送信する
+        // sameSite: 'Strict', // 同一ドメインからしかCookieを送信できなくする
+      };
 
-        const options = {
-          //httpOnly: true, // JavaScriptからアクセスできないようにする(document.cookieで取得もできない)
-          maxAge: 1000 * 60, // 有効期限を設定(ミリ秒)→アプリケーション>Cookieに保存されているの確認
-          // secure: process.env.NODE_ENV === 'production', // HTTPS上でのみ送信する
-          // sameSite: 'Strict', // 同一ドメインからしかCookieを送信できなくする
-        };
+      res.cookie('token', token, options);
 
-        res.cookie('token', token, options);
-
-        return res.redirect('/mypage');
-        res.send({ message: 'やあ' });
-      });
-    }
-  });
+      return res.redirect('/mypage');
+      res.send({ message: 'やあ' });
+    });
+  }
+});
 
 module.exports = router;
