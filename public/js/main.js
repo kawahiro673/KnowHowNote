99e14d0ebdd3f8110b49933ebc8a242832a7a803
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-09 18:59:20 +0900

    module change

--- public/js/main.js
+++ public/js/main.js
@@ -961,8 +961,8 @@ window.addEventListener('DOMContentLoaded', function () {
           console.log(item);
           console.log(item.prevObject[0]);
           console.log(item.prevObject[0].id);
-          id_tmp = item.prevObject[0].id;
-          id_tmp2 = item.prevObject[0].id;
+          let id_tmp = item.prevObject[0].id;
+          let id_tmp2 = item.prevObject[0].id;
           console.log(id);
           $.ajax({
             url: '/mypage/',
