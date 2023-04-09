5db15232c26279b9ed1ff516601e586da6a88edc
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-09 21:24:22 +0900

    module change

--- public/js/tab_func.js
+++ public/js/tab_func.js
@@ -1,3 +1,83 @@
+//タブで必要な関数まとめ
+export const tabCreate = (id, title, res) => {
+  const inputTab = document.createElement('input');
+  inputTab.setAttribute('id', 'TAB-ID' + id);
+  inputTab.setAttribute('type', 'radio');
+  inputTab.setAttribute('name', 'TAB');
+  inputTab.setAttribute('class', 'tab-switch');
+  inputTab.setAttribute('checked', 'checked');
+
+  const labelTab = document.createElement('label');
+  labelTab.setAttribute('class', 'tab-label');
+  labelTab.setAttribute('id', 'tab-ID' + id);
+  labelTab.setAttribute('for', 'TAB-ID' + id);
+  labelTab.style.display = 'block';
+
+  const tabname = document.createElement('p');
+  tabname.setAttribute('class', 'tabname');
+  tabname.setAttribute('id', 'tabname' + id);
+  tabname.innerHTML = title;
+
+  //[✖️]ボタン作成
+  const buttonTab = document.createElement('button');
+  buttonTab.setAttribute('class', 'buttonTab');
+  buttonTab.setAttribute('id', 'button' + id);
+  buttonTab.innerHTML = '×';
+
+  // div要素を生成
+  let div = document.createElement('div');
+  div.className = 'tab-content';
+  div.setAttribute('id', 'Tab-ID' + id);
+  div.setAttribute('value', id);
+  let div1 = document.createElement('div');
+  div1.setAttribute('class', 'title');
+  let p = document.createElement('p');
+  p.setAttribute('class', 'title-txt');
+  p.style.fontSize = '25px';
+  p.style.color = 'black';
+  p.style.textAlign = 'left';
+  p.setAttribute('id', 'tabP' + id);
+  let inputShare = document.createElement('input');
+  inputShare.type = 'submit';
+  inputShare.value = '共有する';
+  let divFade = document.createElement('div');
+  let div2 = document.createElement('div');
+  div2.setAttribute('class', 'form-group');
+  let textarea = document.createElement('textarea');
+  textarea.readOnly = true;
+  textarea.style.height = '500px';
+  let inputEdit = document.createElement('input');
+  inputEdit.type = 'submit';
+  inputEdit.value = '編集する';
+  p.innerHTML = res.title;
+  textarea.innerHTML = res.memo_text;
+  let fadeFont = document.createElement('p');
+  fadeFont.setAttribute('class', 'fade-out-font');
+  fadeFont.innerHTML = `保存が完了いたしました`;
+  fadeFont.classList.add('fadeout');
+  fadeFont.style.visibility = 'hidden';
+  let time = document.createElement('p');
+  time.setAttribute('class', 'updatetime');
+  time.style.color = 'black';
+  time.innerHTML = res.saved_time;
+
+  //要素追加
+  tab.appendChild(inputTab);
+  tab.appendChild(labelTab);
+  labelTab.appendChild(tabname);
+  labelTab.appendChild(buttonTab);
+  tab.appendChild(div);
+  div.appendChild(div1);
+  div.appendChild(divFade);
+  div.appendChild(div2);
+  div1.appendChild(p);
+  div1.appendChild(inputShare);
+  div2.appendChild(textarea);
+  div.appendChild(inputEdit);
+  divFade.appendChild(fadeFont);
+  div.appendChild(time);
+};
+
 //タブエリアの[保存]ボタン押下時
 export const keepButton = (
   id,
