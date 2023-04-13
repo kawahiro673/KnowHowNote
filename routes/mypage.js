fbb8956e310df15c49150397a6d9c13f421fb56d
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-13 21:43:15 +0900

    module change

--- routes/mypage.js
+++ routes/mypage.js
@@ -4,6 +4,10 @@ const pool = require('../db.js');
 const JWT = require('jsonwebtoken');
 const { resetWatchers } = require('nodemon/lib/monitor/watch');
 const { request } = require('express');
+const res = require('express/lib/response');
+const { reject } = require('bcrypt/promises');
+const { off } = require('../db.js');
+const rules = require('nodemon/lib/rules');
 
 router
   .route('/')
@@ -20,7 +24,7 @@ router
         }
       );
     } else if (req.body.data === 'tab') {
-      if (req.body.flg == 'clickTab') {
+      if (req.body.flg === 'clickTab') {
         const token = req.cookies.token;
         const decoded = JWT.verify(token, 'SECRET_KEY');
         let promise = new Promise((resolve, reject) => {
@@ -76,29 +80,63 @@ router
             console.error(error);
             res.status(500).send('Internal Server Error.(clickTab)');
           });
-      } else if (req.body.flg == 'updateFocus') {
+      } else if (req.body.flg === 'updateFocus') {
         const token = req.cookies.token;
         const decoded = JWT.verify(token, 'SECRET_KEY');
-        pool.query(
-          'SELECT * FROM register_user WHERE Email = ?;',
-          [decoded.email],
-          (error, resultDecoded) => {
-            pool.query(
-              'UPDATE tab_hold SET focus = ?, pass = ? where id = ?',
-              [1, req.body.pass, req.body.id],
-              (error, result) => {
-                pool.query(
-                  'UPDATE tab_hold SET focus = ? where id != ? AND (UserID = ?)',
-                  [0, req.body.id, resultDecoded[0].id],
-                  (error, results) => {
+        let promise = new Promise((resolve, reject) => {
+          resolve();
+        });
+        promise
+          .then(() => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'SELECT * FROM register_user WHERE Email = ?;',
+                [decoded.email],
+                (error, resultDecoded) => {
+                  if (error) {
+                    reject();
+                  } else {
+                    resolve(resultDecoded);
+                  }
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'UPDATE tab_hold SET focus = ?, pass = ? where id = ?',
+                [1, req.body.pass, req.body.id],
+                (error, result) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve({ result: result, resultDecoded: resultDecoded });
+                  }
+                }
+              );
+            });
+          })
+          .then(({ result, resultDecoded }) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'UPDATE tab_hold SET focus = ? where id != ? AND (UserID = ?)',
+                [0, req.body.id, resultDecoded[0].id],
+                (error, results) => {
+                  if (error) {
+                    reject(error);
+                  } else {
                     res.send({ response: result });
                   }
-                );
-              }
-            );
-          }
-        );
-      } else if (req.body.flg == 'updateOrder') {
+                }
+              );
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error.(addOrder)');
+          });
+      } else if (req.body.flg === 'updateOrder') {
         pool.query(
           'UPDATE tab_hold SET tabOrder = ? where id = ?',
           [req.body.order, req.body.id],
@@ -106,86 +144,207 @@ router
             res.send({ response: results });
           }
         );
-      } else if (req.body.flg == 'tabDesc') {
+      } else if (req.body.flg === 'tabDesc') {
         const token = req.cookies.token;
         const decoded = JWT.verify(token, 'SECRET_KEY');
-        pool.query(
-          'SELECT * FROM register_user WHERE Email = ?;',
-          [decoded.email],
-          (error, resultDecoded) => {
-            pool.query(
-              'SELECT * FROM tab_hold WHERE UserID = ? ORDER BY tabOrder;',
-              [resultDecoded[0].id],
-              (error, results) => {
-                res.send({ response: results });
-              }
-            );
-          }
-        );
-      } else if (req.body.flg == 'info') {
+        let promise = new Promise((resolve, reject) => {
+          resolve();
+        });
+        promise
+          .then(() => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'SELECT * FROM register_user WHERE Email = ?;',
+                [decoded.email],
+                (error, resultDecoded) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(resultDecoded);
+                  }
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise(() => {
+              pool.query(
+                'SELECT * FROM tab_hold WHERE UserID = ? ORDER BY tabOrder;',
+                [resultDecoded[0].id],
+                (error, results) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    res.send({ response: results });
+                  }
+                }
+              );
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error.(tabDesc)');
+          });
+      } else if (req.body.flg === 'info') {
         pool.query('SELECT * FROM tab_hold;', (error, result) => {
           res.send({ response: result });
         });
-      } else if (req.body.flg == 'focusTab') {
+      } else if (req.body.flg === 'focusTab') {
         const token = req.cookies.token;
         const decoded = JWT.verify(token, 'SECRET_KEY');
-        pool.query(
-          'SELECT * FROM register_user WHERE Email = ?;',
-          [decoded.email],
-          (error, resultDecoded) => {
-            pool.query(
-              'select * from tab_hold where focus = 1 AND (UserID = ?);',
-              [resultDecoded[0].id],
-              (error, result) => {
-                res.send({ response: result[0] });
-              }
-            );
-          }
-        );
-      } else if (req.body.flg == 'tabDel') {
-        const token = req.cookies.token;
-        const decoded = JWT.verify(token, 'SECRET_KEY');
-        pool.query(
-          'SELECT * FROM register_user WHERE Email = ?;',
-          [decoded.email],
-          (error, resultDecoded) => {
-            pool.query(
-              'SELECT * FROM it_memo WHERE id = ?',
-              [req.body.id],
-              (error, resultFocus) => {
-                pool.query(
-                  'DELETE from tab_hold where id = ?',
-                  [req.body.id],
-                  (error, result) => {
-                    pool.query(
-                      'UPDATE tab_hold SET tabOrder = tabOrder - 1 WHERE tabOrder > ? AND (UserID = ?); ',
-                      [req.body.order, resultDecoded[0].id],
-                      (error, results) => {
-                        res.send({ response: result });
-                      }
-                    );
+        let promise = new Promise((resolve, reject) => {
+          resolve();
+        });
+        promise
+          .then(() => {
+            return new Promise(() => {
+              pool.query(
+                'SELECT * FROM register_user WHERE Email = ?;',
+                [decoded.email],
+                (error, resultDecoded) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(resultDecoded);
                   }
-                );
-              }
-            );
-          }
-        );
-      } else if (req.body.flg == 'tabAdd') {
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'select * from tab_hold where focus = 1 AND (UserID = ?);',
+                [resultDecoded[0].id],
+                (error, result) => {
+                  res.send({ response: result[0] });
+                }
+              );
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error.(focusTab)');
+          });
+      } else if (req.body.flg === 'tabDel') {
         const token = req.cookies.token;
         const decoded = JWT.verify(token, 'SECRET_KEY');
-        pool.query(
-          'SELECT * FROM register_user WHERE Email = ?;',
-          [decoded.email],
-          (error, resultDecoded) => {
-            pool.query(
-              'INSERT into tab_hold(id, tabTitle, pass, UserID) values(?, ?, ?, ?);',
-              [req.body.id, req.body.title, req.body.pass, resultDecoded[0].id],
-              (error, results) => {
-                res.send({ response: results });
-              }
-            );
-          }
-        );
+        let promise = new Promise((resolve, reject) => {
+          resolve();
+        });
+
+        promise
+          .then(() => {
+            return new Promise(() => {
+              pool.query(
+                'SELECT * FROM register_user WHERE Email = ?;',
+                [decoded.email],
+                (error, resultDecoded) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(resultDecoded);
+                  }
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'SELECT * FROM it_memo WHERE id = ?',
+                [req.body.id],
+                (error, resultFocus) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(resultDecoded);
+                  }
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'DELETE from tab_hold where id = ?',
+                [req.body.id],
+                (error, result) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve({ result: result, resultDecoded: resultDecoded });
+                  }
+                }
+              );
+            });
+          })
+          .then(({ result, resultDecoded }) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'UPDATE tab_hold SET tabOrder = tabOrder - 1 WHERE tabOrder > ? AND (UserID = ?); ',
+                [req.body.order, resultDecoded[0].id],
+                (error, results) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    res.send({ response: result });
+                  }
+                }
+              );
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error.(tabDel)');
+          });
+      } else if (req.body.flg === 'tabAdd') {
+        const token = req.cookies.token;
+        const decoded = JWT.verify(token, 'SECRET_KEY');
+        let promise = new Promise((resolve, reject) => {
+          resolve();
+        });
+
+        promise
+          .then(() => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'SELECT * FROM register_user WHERE Email = ?;',
+                [decoded.email],
+                (error, resultDecoded) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(resultDecoded);
+                  }
+                }
+              );
+            });
+          })
+          .then((resultDecoded) => {
+            return new Promise((resolve, reject) => {
+              pool.query(
+                'INSERT into tab_hold(id, tabTitle, pass, UserID) values(?, ?, ?, ?);',
+                [
+                  req.body.id,
+                  req.body.title,
+                  req.body.pass,
+                  resultDecoded[0].id,
+                ],
+                (error, results) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    res.send({ response: results });
+                  }
+                }
+              );
+            });
+          })
+          .catch(() => {
+            console.error(error);
+            res.status(500).send('Internal Server Error.(tabAdd)');
+          });
       }
     } else if (req.body.data === 'addOrder') {
       const token = req.cookies.token;
