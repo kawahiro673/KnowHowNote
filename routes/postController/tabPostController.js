0daf1fb32f2a51ba78ea5e08a92acab79facc493
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-17 00:08:31 +0900

    module change

--- routes/postController/tabPostController.js
+++ routes/postController/tabPostController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const pool = require('../db.js');
+const JWT = require('jsonwebtoken');
+const { reject } = require('bcrypt/promises');
 
 router.post((req, res) => {
   if (req.body.data === 'tab') {
