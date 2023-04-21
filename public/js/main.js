77a65c54c417a4bd2dbd92cfc3046d5970c83f65
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-21 22:54:48 +0900

    new file bottun

--- public/js/main.js
+++ public/js/main.js
@@ -515,6 +515,7 @@ createfilebutton.addEventListener(
       e.stopPropagation();
       fileInputExistFlg = true;
       newFileCreateFunc(id, fileInputExistFlg, tabIdArray);
+      fileInputExistFlg = false;
     }
   },
   false
--- public/js/newFileCreate.js
+++ public/js/newFileCreate.js
@@ -4,7 +4,6 @@ import { updateTime } from './tab_func.js';
 import { listCreate, fileClick } from './main.js';
 
 export const newFileCreateFunc = (id, fileInputExistFlg, tabIdArray) => {
-  fileInputExistFlg = true;
   const li = document.createElement('li');
   const span = document.createElement('span');
   li.setAttribute('class', 'last');
