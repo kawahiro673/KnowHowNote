27dec1b1699c0f6b02666510ac1f1477c027c288
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-22 20:17:20 +0900

    pass upfdate

--- public/js/share_contextmenu.js
+++ public/js/share_contextmenu.js
@@ -1,6 +1,10 @@
 export const shareContextmenu = () => {
   $('.sharenote').on('contextmenu', (e) => {
     console.log('シェア右クリック');
-    console.log(this);
+    console.log(`"${$(this).html()}" を右クリックしました`);
+    let share = {
+      shareTitle: $(this).html(),
+      shareThis: this,
+    };
   });
 };
