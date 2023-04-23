8ae981d1591e8c8fad5f0b378e5fa94d81839772
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-23 22:27:23 +0900

    pass upfdate

--- routes/mypage.js
+++ routes/mypage.js
@@ -415,8 +415,8 @@ router
         .then((resultDecoded) => {
           return new Promise((resolve, reject) => {
             pool.query(
-              'select * from folder WHERE (Type NOT LIKE ?)  AND(UserID = ?) order by folder_order ASC;',
-              ['Share', resultDecoded[0].id],
+              'select * from folder WHERE (Type != "Share") AND (UserID = ?) order by folder_order ASC ',
+              [resultDecoded[0].id],
               (error, results) => {
                 if (error) {
                   reject(error);
@@ -430,8 +430,8 @@ router
         .then(({ results, resultDecoded }) => {
           return new Promise((resolve, reject) => {
             pool.query(
-              'SELECT * FROM it_memo WHERE (Type NOT LIKE ?) AND UserID = ? ORDER BY folder_order ASC;',
-              ['Share', resultDecoded[0].id],
+              'select * from it_memo WHERE (Type != "Share") AND (UserID = ?) order by folder_order ASC',
+              [resultDecoded[0].id],
               (error, result) => {
                 if (error) {
                   reject(error);
