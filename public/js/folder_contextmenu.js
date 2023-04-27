3e97c745873e14a670f4864617eb94dedd15efb3
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-04-27 21:31:11 +0900

    expandable

--- public/js/folder_contextmenu.js
+++ public/js/folder_contextmenu.js
@@ -168,6 +168,7 @@ const folderNameChange = (folder) => {
   document
     .getElementById('inputTab')
     .addEventListener('focus', (event) => event.target.select());
+  document.getElementById('inputTab').click();
 
   //Enter押下で変更する
   inputTab.addEventListener('keypress', function (e) {
