9e5d6ab8580ce919b3d6e9122da3f81517d9cde6
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-06 03:12:49 +0900

    main UI

--- public/js/label_contextmenu.js
+++ public/js/label_contextmenu.js
@@ -22,13 +22,46 @@ export const labelContextmenu = (tabIdArray) => {
     };
 
     document.getElementById('pink').addEventListener('click', () => {
-      tabLabelsColor(label.id);
+      tabLabelsColor(label.id, '#FF00FF');
+    });
+    document.getElementById('red').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#FF0000');
+    });
+    document.getElementById('orange').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#FFA500');
+    });
+    document.getElementById('yellow').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#FFFF00');
+    });
+    document.getElementById('yellow-green').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#98FB98');
+    });
+    document.getElementById('green').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#66CDAA');
+    });
+    document.getElementById('sky-blue').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#AFEEEE');
+    });
+    document.getElementById('blue').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#0000FF');
+    });
+    document.getElementById('purple').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#800080');
+    });
+    document.getElementById('brown').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#B8860B');
+    });
+    document.getElementById('gray').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#A9A9A9');
+    });
+    document.getElementById('black').addEventListener('click', () => {
+      tabLabelsColor(label.id, '#000000');
     });
   });
 };
 
-const tabLabelsColor = (id) => {
+const tabLabelsColor = (id, color) => {
   const label = document.getElementById(`tab-ID${id}`);
   // // 各.tab-labelにランダムな色を割り当てる
-  label.style.setProperty('--tab-label-background-color', '#FF00FF');
+  label.style.setProperty('--tab-label-background-color', color);
 };
