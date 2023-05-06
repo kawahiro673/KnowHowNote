9f01efa6cf22e5844e394572e66854976643a47f
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-06 23:57:44 +0900

    main UI

--- public/js/main.js
+++ public/js/main.js
@@ -326,7 +326,7 @@ function tabUpload() {
       };
       await createTheFirstTab();
       await tabFocusOn();
-      for (let i = 0; i < 20; i++) {
+      for (let i = 0; i < 22; i++) {
         const imageContainer = document.createElement('div');
         imageContainer.classList.add('image-container');
 
