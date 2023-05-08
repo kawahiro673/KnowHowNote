ab55efec1aaa504e559562272554f61cb9458986
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-08 22:31:02 +0900

    main UI

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,66 @@
+//プロフィールのポップアップ
+document.getElementById('profile').addEventListener('click', () => {
+  document.getElementById('popup-overlay_profile').style.display = 'block';
+});
+
+document.getElementById('pop-delete_profile').addEventListener('click', (e) => {
+  e.preventDefault(); // リンクのデフォルトの動作を無効化
+  document.getElementById('popup-overlay_profile').style.display = 'none';
+});
+
+document
+  .getElementById('popup-overlay_profile')
+  .addEventListener('click', (e) => {
+    const popup = document.getElementById('popup-overlay_profile');
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+
+//共有履歴のポップアップ
+document.getElementById('share-history').addEventListener('click', () => {
+  document.getElementById('popup-overlay_share-history').style.display =
+    'block';
+});
+
+document
+  .getElementById('pop-delete_share-history')
+  .addEventListener('click', (e) => {
+    e.preventDefault(); // リンクのデフォルトの動作を無効化
+    document.getElementById('popup-overlay_share-history').style.display =
+      'none';
+  });
+
+document
+  .getElementById('popup-overlay_share-history')
+  .addEventListener('click', (e) => {
+    const popup = document.getElementById('popup-overlay_share-history');
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+
+//使い方のポップアップ
+document.getElementById('explanation').addEventListener('click', () => {
+  document.getElementById('popup-overlay_explanation').style.display = 'block';
+});
+
+document
+  .getElementById('pop-delete_explanation')
+  .addEventListener('click', (e) => {
+    e.preventDefault(); // リンクのデフォルトの動作を無効化
+    document.getElementById('popup-overlay_explanation').style.display = 'none';
+  });
+
+document
+  .getElementById('popup-overlay_explanation')
+  .addEventListener('click', (e) => {
+    const popup = document.getElementById('popup-overlay_explanation');
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+
 //バージョンのポップアップ
 document.getElementById('version').addEventListener('click', () => {
   document.getElementById('popup-overlay_version').style.display = 'block';
@@ -27,6 +90,46 @@ document.getElementById('pop-delete_inquiry').addEventListener('click', (e) => {
   document.getElementById('popup-overlay_inquiry').style.display = 'none';
 });
 
+//ログアウトポップアップ
+document
+  .getElementById('popup-overlay_logout')
+  .addEventListener('click', () => {
+    //はいを押した場合(true)
+    document.getElementById('popup-overlay_logout').style.display = 'block';
+  });
+
+document.getElementById('yes-button-logout').addEventListener('click', () => {
+  $.ajax({
+    url: '/mypage/',
+    type: 'POST',
+    dataType: 'Json',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      flg: 'cookiedelete',
+    }),
+    success: function (res) {},
+  });
+});
+
+document.getElementById('pop-delete_logout').addEventListener('click', (e) => {
+  e.preventDefault(); // リンクのデフォルトの動作を無効化
+  document.getElementById('popup-overlay_logout').style.display = 'none';
+});
+
+document.getElementById('no-button-logout').addEventListener('click', (e) => {
+  e.preventDefault(); // リンクのデフォルトの動作を無効化
+  document.getElementById('popup-overlay_logout').style.display = 'none';
+});
+
+document
+  .getElementById('popup-overlay_logout')
+  .addEventListener('click', (e) => {
+    const popup = document.getElementById('popup-overlay_logout');
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+
 //[全削除]ボタン押下時。ノート,フォルダ,タブ全て削除
 document
   .getElementById('popup-overlay_delete')
@@ -35,7 +138,7 @@ document
     document.getElementById('popup-overlay_inquiry').style.display = 'block';
   });
 
-document.getElementById('yes-button').addEventListener('click', () => {
+document.getElementById('yes-button-delete').addEventListener('click', () => {
   $.ajax({
     url: '/mypage/',
     type: 'POST',
@@ -62,7 +165,7 @@ document.getElementById('pop-delete_delete').addEventListener('click', (e) => {
   document.getElementById('popup-overlay_delete').style.display = 'none';
 });
 
-document.getElementById('no-button').addEventListener('click', (e) => {
+document.getElementById('no-button-delete').addEventListener('click', (e) => {
   e.preventDefault(); // リンクのデフォルトの動作を無効化
   document.getElementById('popup-overlay_delete').style.display = 'none';
 });
@@ -76,16 +179,16 @@ document
     }
   });
 
-//[ログアウト]押下後、サーバーでCookieを削除
-document.getElementById('logout').addEventListener('click', () => {
-  $.ajax({
-    url: '/mypage/',
-    type: 'POST',
-    dataType: 'Json',
-    contentType: 'application/json',
-    data: JSON.stringify({
-      flg: 'cookiedelete',
-    }),
-    success: function (res) {},
-  });
-});
+// //[ログアウト]押下後、サーバーでCookieを削除
+// document.getElementById('logout').addEventListener('click', () => {
+//   $.ajax({
+//     url: '/mypage/',
+//     type: 'POST',
+//     dataType: 'Json',
+//     contentType: 'application/json',
+//     data: JSON.stringify({
+//       flg: 'cookiedelete',
+//     }),
+//     success: function (res) {},
+//   });
+// });
