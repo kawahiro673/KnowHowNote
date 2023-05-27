14a9f2dd388279609bf5e959900a116bac86ab74
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-28 00:20:56 +0900

    list loader screen

--- public/js/label_contextmenu.js
+++ public/js/label_contextmenu.js
@@ -1,19 +1,5 @@
-const conme = document.getElementById('contextmenu');
-const conme2 = document.getElementById('contextmenu2');
-const conme3 = document.getElementById('contextmenu3');
-const conme4 = document.getElementById('contextmenu4');
-const conme5 = document.getElementById('contextmenu5');
-
 export const labelContextmenu = () => {
   $('.tab-label').on('contextmenu', function (e) {
-    conme5.style.left = e.pageX + 'px';
-    conme5.style.top = e.pageY + 'px';
-    conme5.style.display = 'block';
-    conme.style.display = 'none';
-    conme2.style.display = 'none';
-    conme3.style.display = 'none';
-    conme4.style.display = 'none';
-
     const idValue = this.id;
     let label = {
       title: $(this).html(),
