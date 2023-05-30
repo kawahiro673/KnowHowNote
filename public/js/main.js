6626564150874a101e182fc7d3339c6c55d60a4c
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-31 01:46:16 +0900

    list share contextmenu

--- public/js/main.js
+++ public/js/main.js
@@ -408,14 +408,14 @@ async function titleClick(id, title) {
               .getElementById(`keep-note-btn${id}`)
               .addEventListener('click', () => {
                 keepButtonClick(id);
-                document.getElementById(`tab-ID${id}`).style.color = 'black';
+                document.getElementById(`tab-ID${id}`).style.color = null;
               });
 
             document
               .getElementById(`cancel-note-btn${id}`)
               .addEventListener('click', () => {
                 cancelButtonClick(id);
-                document.getElementById(`tabname${id}`).style.color = 'black';
+                document.getElementById(`tabname${id}`).style.color = null;
               });
           };
 
