fc42b944c72d5b4ab84be307be6ab208a951cd45
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-05-31 01:27:39 +0900

    list share contextmenu

--- public/js/main.js
+++ public/js/main.js
@@ -408,14 +408,16 @@ async function titleClick(id, title) {
               .getElementById(`keep-note-btn${id}`)
               .addEventListener('click', () => {
                 keepButtonClick(id);
-                document.getElementById(`tabname${id}`).style.color = 'black';
+                document.getElementById(`tab-ID${id}`).remove('tab-label');
+                document.getElementById(`tab-ID${id}`).add('tab-label');
               });
 
             document
               .getElementById(`cancel-note-btn${id}`)
               .addEventListener('click', () => {
                 cancelButtonClick(id);
-                document.getElementById(`tabname${id}`).style.color = 'black';
+                document.getElementById(`tab-ID${id}`).remove('tab-label');
+                document.getElementById(`tab-ID${id}`).add('tab-label');
               });
           };
 
