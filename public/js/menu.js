ec9e76d2f245c69725e3aa690b950652806a17d9
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-06-11 20:44:51 +0900

    hashedId

--- public/js/menu.js
+++ public/js/menu.js
@@ -66,9 +66,9 @@ document.getElementById('share-history').addEventListener('click', () => {
           document.getElementById('share-history-list').appendChild(p);
         });
         if (res.shareResult.length === 0) {
-          console.log('作成されていません');
           const p = document.createElement('p');
           p.innerHTML = '共有履歴がありません';
+          p.setAttribute('class', 'no-share-user');
           document.getElementById('share-history-list').appendChild(p);
         }
       }
