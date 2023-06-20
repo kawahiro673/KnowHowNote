b16014e38214c2955ea36fa0e03b0841e826ba41
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-06-21 01:13:53 +0900

    tab screen button

--- public/js/tab_func.js
+++ public/js/tab_func.js
@@ -25,6 +25,7 @@ export const tabCreate = (id, title, res) => {
   tabname.setAttribute('class', 'tabname');
   tabname.setAttribute('id', 'tabname' + id);
   tabname.style.fontSize = '15px';
+  tabname.style.zIndex = '1';
   tabname.innerHTML = title;
 
   //[✖️]ボタン作成
