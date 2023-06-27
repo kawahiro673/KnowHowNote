2bf9e288017f33a6a3bd226a95353ec1b996bc4f
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-06-27 12:24:25 +0900

    tab screen button

--- public/js/main.js
+++ public/js/main.js
@@ -380,7 +380,7 @@ async function tabScreenOptions(id, title) {
             document.getElementById(`fade${id}`).style.opacity = '1';
             document.getElementById(`fade${id}`).style.color = 'red';
             document.getElementById(`fade${id}`).textContent =
-              '※現在編集中です。編集完了後【保存する】ボタンを押してください';
+              '※編集完了後【保存する】ボタンを押してください';
 
             document.getElementById(`share-button-${id}`).disabled = true;
             document.getElementById(
