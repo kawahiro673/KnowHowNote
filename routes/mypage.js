5344b3579f9a9095844f8ed2603811a233c45709
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-07-11 09:43:49 +0900

    Update mypage.js

--- routes/mypage.js
+++ routes/mypage.js
@@ -1053,17 +1053,7 @@ router
               }
             );
           });
-        }) .then((resultDecoded) => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'SELECT * FROM group_list WHERE UserID = ?;',
-              [resultDecoded[0].id],
-              (error, result) => {
-                res.send({ groupResults : result });
-              }
-            );
-          });
-        }) .catch((error) => {
+        }).catch((error) => {
           console.error(error);
           res.status(500).json({ message: error.message, nothing });
         });
