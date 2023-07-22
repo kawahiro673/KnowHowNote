eaaf6e9c849a3d2535d5fb06362d64d3150e004f
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-07-22 21:00:14 +0900

    Commit message

--- public/js/MENU/friend-list.js
+++ public/js/MENU/friend-list.js
@@ -1,7 +1,7 @@
 //MENUボタンのフレンドリストの実装
 
 import { hashedIdGet } from '../main.js';
-import { friendListGroupChange } from '../group-list.js';
+import { friendListGroupChange } from './group-list.js';
 
 export const friendListDelete = (name) => {
   document.getElementById('popup-overlay_friend-delete-q').style.display =
