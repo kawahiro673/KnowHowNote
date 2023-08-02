712ac67ff7361598d1772972f57c1f8efd8a6ebb
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-02 10:40:36 +0900

    Update tabPostController.js

--- routes/postController/tabPostController.js
+++ routes/postController/tabPostController.js
@@ -296,51 +296,123 @@ router.post('/', (req, res) => {
       });
   } else if (req.body.flg === 'tabAdd') {
     //タブが生成されていなければ実施(INSERT処理のため)
-    if (!req.body.isSomething) {
-      const token = req.cookies.token;
-      const decoded = JWT.verify(token, 'SECRET_KEY');
-      let promise = new Promise((resolve, reject) => {
-        resolve();
-      });
+    // if (!req.body.isSomething) {
+    //   const token = req.cookies.token;
+    //   const decoded = JWT.verify(token, 'SECRET_KEY');
+    //   let promise = new Promise((resolve, reject) => {
+    //     resolve();
+    //   });
 
-      promise
-        .then(() => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'SELECT * FROM register_user WHERE UserName = ?;',
-              [decoded.userName],
-              (error, resultDecoded) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  resolve(resultDecoded);
-                }
-              }
-            );
-          });
-        })
-        .then((resultDecoded) => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'INSERT into tab_hold(id, tabTitle, UserID) values(?, ?, ?);',
-              [req.body.id, req.body.title, resultDecoded[0].id],
-              (error, result) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  res.send({ msg: '成功しました' });
-                }
-              }
-            );
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Internal Server Error.(tabAdd)');
-        });
-    } else {
-      res.end(); // 何も送信しない
+    //   promise
+    //     .then(() => {
+    //       return new Promise((resolve, reject) => {
+    //         pool.query(
+    //           'SELECT * FROM register_user WHERE UserName = ?;',
+    //           [decoded.userName],
+    //           (error, resultDecoded) => {
+    //             if (error) {
+    //               reject(error);
+    //             } else {
+    //               resolve(resultDecoded);
+    //             }
+    //           }
+    //         );
+    //       });
+    //     })
+    //     .then((resultDecoded) => {
+    //       return new Promise((resolve, reject) => {
+    //         pool.query(
+    //           'INSERT into tab_hold(id, tabTitle, UserID) values(?, ?, ?);',
+    //           [req.body.id, req.body.title, resultDecoded[0].id],
+    //           (error, result) => {
+    //             if (error) {
+    //               reject(error);
+    //             } else {
+    //               res.send({ msg: '成功しました' });
+    //             }
+    //           }
+    //         );
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //       res.status(500).send('Internal Server Error.(tabAdd)');
+    //     });
+    // } else {
+    //   res.end(); // 何も送信しない
+    // }
+const token = req.cookies.token;
+const decoded = JWT.verify(token, 'SECRET_KEY');
+
+const promise = new Promise((resolve, reject) => {
+  pool.query(
+    'SELECT * FROM register_user WHERE UserName = ?;',
+    [decoded.userName],
+    (error, resultDecoded) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(resultDecoded);
+      }
     }
+  );
+});
+
+promise
+  .then((resultDecoded) => {
+    if (!req.body.isSomething) {
+      return new Promise((resolve, reject) => {
+        pool.query(
+          'INSERT into tab_hold(id, tabTitle, UserID) values(?, ?, ?);',
+          [req.body.id, req.body.title, resultDecoded[0].id],
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(resultDecoded);
+            }
+          }
+        );
+      });
+    } else {
+      return resultDecoded; // 何もしない場合はそのままresultDecodedを返す
+    }
+  })
+  .then((resultDecoded) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'UPDATE tab_hold SET tabOrder = ?, focus = 1 where id = ?',
+        [req.body.order, req.body.id],
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(resultDecoded);
+          }
+        }
+      );
+    });
+  })
+  .then((resultDecoded) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'UPDATE tab_hold SET focus = 0 where id != ? AND (UserID = ?)',
+        [req.body.id, resultDecoded[0].id],
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            res.send({ msg: '成功しました' });
+          }
+        }
+      );
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Internal Server Error.(tabAdd)');
+  });
+
   } else if (req.body.flg === 'labelColorUpdate') {
     pool.query(
       'UPDATE it_memo SET tab_color = ? WHERE id = ? ',
