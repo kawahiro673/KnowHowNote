59f536eb566682bae0a9676aea825699958f54e4
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-08-06 23:19:01 +0900

    Commit message

--- public/js/jQueryUI_func.js
+++ public/js/jQueryUI_func.js
@@ -289,19 +289,26 @@ export const jQueryUIOptionsFunc = () => {
 function updateLastClasses(element) {
   const siblings = element.parentNode.children;
   const isLastChild = element === siblings[siblings.length - 1];
+  let hasLastClassSibling = false;
 
   // 同階層の兄弟要素に last クラスがある場合、削除
   for (const sibling of siblings) {
     if (sibling !== element && sibling.classList.contains('last')) {
       sibling.classList.remove('last');
+      hasLastClassSibling = true;
     }
   }
 
   if (isLastChild) {
-    element.classList.add('last'); // 要素が一番下の場合、last クラスを追加
+    element.classList.add('last'); // ① 要素が一番下の場合、last クラスを追加
   } else {
     if (element.classList.contains('last')) {
-      element.classList.remove('last'); // 要素が一番下でなく、自身に last クラスがある場合、last クラスを削除
+      element.classList.remove('last'); // ③ 要素が一番下でなく、自身に last クラスがある場合、last クラスを削除
     }
+  }
+
+  if (!hasLastClassSibling) {
+    const lastSibling = siblings[siblings.length - 1];
+    lastSibling.classList.add('last'); // ④ 同階層の兄弟要素に last クラスがなければ、一番下の要素に last クラスを追加
   }
 }
