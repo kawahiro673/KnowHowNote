b486f9185fd50f40a18e5620f2c9e7d134272569
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-07 15:30:15 +0900

    Update share_contextmenu.js

--- public/js/contextmenu/share_contextmenu.js
+++ public/js/contextmenu/share_contextmenu.js
@@ -115,45 +115,12 @@ const mynoteAddFunc = (id) => {
       id,
     }),
     success: function (res) {
-      // const li = document.createElement('li');
-      // const span = document.createElement('span');
-      // li.setAttribute('class', 'last');
-      // li.setAttribute('class', `parent0`);
-      // li.setAttribute('id', `li${id}`);
-      // span.classList.add('list_title', 'file');
-
-      // span.setAttribute('id', `file${res.fileResult.id}`);
-      // span.setAttribute('value', res.fileResult.id);
-      // span.innerHTML = res.fileResult.title;
-
-      // document.getElementById('0').appendChild(li);
-      // li.appendChild(span);
-
-      // //MyPage追加後の順番
-      // let elements = document.getElementsByClassName(`parent0`);
-      // let order = [].slice.call(elements).indexOf(span.parentNode);
-      // order++;
-      // $.ajax({
-      //   url: '/notePostController/',
-      //   type: 'POST',
-      //   dataType: 'Json',
-      //   contentType: 'application/json',
-      //   data: JSON.stringify({
-      //     flg: 'newNote',
-      //     pattern: 'order',
-      //     id: res.fileResult.id,
-      //     order,
-      //   }),
-      //   success: function (res) {
-          console.log(res.order);
           //一度listを全て削除して、再び新しく追加している→jQueryUIがうまく適用されないため
           const node = document.getElementById('0');
           while (node.firstChild) {
             node.removeChild(node.firstChild);
           }
           listCreate();
-      //   },
-      // });
     },
   });
 };
