bc6bc2336b7bf07ffe14d9400f17c63ea5e7abba
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-08 14:44:27 +0900

    Update menu.js

--- public/js/MENU/menu.js
+++ public/js/MENU/menu.js
@@ -151,7 +151,9 @@ document.getElementById('share-history').addEventListener('click', () => {
     }),
     success: function (res) {
       if (res.shareResult === null) {
-        alert('共有履歴はございません');
+         document.getElementById(
+          'share-history-h1'
+        ).innerHTML = '共有履歴がありません';
       } else {
         const shareHistoryTable = document.getElementById(
           'share-history-table'
@@ -167,6 +169,9 @@ document.getElementById('share-history').addEventListener('click', () => {
               .getElementsByTagName('p').length > 0
           )
         ) {
+           document.getElementById(
+          'share-history-h1'
+        ).innerHTML = '直近100件まで表示します';
           const table = document.createElement('table');
           table.setAttribute('border', '1');
           table.setAttribute('id', 'share-history-table');
