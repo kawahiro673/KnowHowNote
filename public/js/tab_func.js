ab75cea33f8563aac87119aace70dd4e05fbe727
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-10 15:03:15 +0900

    Update tab_func.js

--- public/js/tab_func.js
+++ public/js/tab_func.js
@@ -192,24 +192,24 @@ export const keepButtonClick = (id) => {
 ////////////////////////////////////////////////////////////////
 
 //タブエリアの[取り消し]ボタン押下時
-export const cancelButtonClick = (id) => {
-  document.getElementById('popup-overlay_keep-cancel').style.display = 'block';
+export const cancelButtonClick = async (id) => {
+  // document.getElementById('popup-overlay_keep-cancel').style.display = 'block';
 
-  const nouhauKeepCancelButtonListener = () => {
-    nouhauKeepCancel(id);
-    document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
-    document
-      .getElementById('yes-button-keep-cancel')
-      .removeEventListener('click', nouhauKeepCancelButtonListener);
-  };
+  // const nouhauKeepCancelButtonListener = () => {
+  //   nouhauKeepCancel(id);
+  //   document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
+  //   document
+  //     .getElementById('yes-button-keep-cancel')
+  //     .removeEventListener('click', nouhauKeepCancelButtonListener);
+  // };
 
-  document
-    .getElementById('yes-button-keep-cancel')
-    .addEventListener('click', nouhauKeepCancelButtonListener);
-};
-
-const nouhauKeepCancel = (id) => {
-  $.ajax({
+  // document
+  //   .getElementById('yes-button-keep-cancel')
+  //   .addEventListener('click', nouhauKeepCancelButtonListener);
+  console.log('やあ');
+   const result = await answerPopUp('取り消し','本当に編集を取り消しますか？保存していないものは取り消されます');
+   if (result === true) {
+       $.ajax({
     url: '/notePostController/',
     type: 'POST',
     dataType: 'Json',
@@ -233,21 +233,51 @@ const nouhauKeepCancel = (id) => {
   document.getElementById(`tabname${id}`).style.color = 'black';
 
   resultPopUp('取り消し', '取り消しました');
+  } else {
+    // 「いいえ」が押された場合の処理 おそらくポップが閉じる
+  }
 };
 
-document
-  .getElementById('pop-delete_keep-cancel')
-  .addEventListener('click', (e) => {
-    e.preventDefault(); // リンクのデフォルトの動作を無効化
-    document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
-  });
+// const nouhauKeepCancel = (id) => {
+//   $.ajax({
+//     url: '/notePostController/',
+//     type: 'POST',
+//     dataType: 'Json',
+//     contentType: 'application/json',
+//     data: JSON.stringify({
+//       flg: 'info',
+//       id,
+//     }),
+//     success: function (res) {
+//       document.getElementById(`textarea${id}`).value = res.fileResult.memo_text;
+//     },
+//   });
+//   document.getElementById(`tabP${id}`).style.display = 'block';
+//   document.getElementById(`keep-note-btn${id}`).style.display = 'none';
+//   document.getElementById(`cancel-note-btn${id}`).style.display = 'none';
+//   document.getElementById(`titletext${id}`).style.display = 'none';
+//   document.getElementById(`edit-note-btn${id}`).style.display = 'block';
+//   document.getElementById(`share-button-${id}`).style.display = 'block';
+//   document.getElementById(`textarea${id}`).readOnly = true;
+//   document.getElementById(`fade${id}`).style.opacity = '0';
+//   document.getElementById(`tabname${id}`).style.color = 'black';
 
-document
-  .getElementById('no-button-keep-cancel')
-  .addEventListener('click', (e) => {
-    e.preventDefault(); // リンクのデフォルトの動作を無効化
-    document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
-  });
+//   resultPopUp('取り消し', '取り消しました');
+// };
+
+// document
+//   .getElementById('pop-delete_keep-cancel')
+//   .addEventListener('click', (e) => {
+//     e.preventDefault(); // リンクのデフォルトの動作を無効化
+//     document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
+//   });
+
+// document
+//   .getElementById('no-button-keep-cancel')
+//   .addEventListener('click', (e) => {
+//     e.preventDefault(); // リンクのデフォルトの動作を無効化
+//     document.getElementById('popup-overlay_keep-cancel').style.display = 'none';
+//   });
 
 ////////////////////////////////////////////////////////////////
 /////////////////////////////共有機能////////////////////////////
