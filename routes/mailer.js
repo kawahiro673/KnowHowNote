6e9281cd0e581d02761ee9114360fa3816525c38
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-10 09:38:18 +0900

    Update mailer.js

--- routes/mailer.js
+++ routes/mailer.js
@@ -4,20 +4,36 @@ const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'akanuma.9099@gmail.com',
-    pass: '9099asaa',
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'akanuma.9099@gmail.com',
+//     pass: '9099asaa',
+//   },
+// });
+
+const transport = {
+  service: "gmail",
+  auth,
+};
+
+//認証情報
+const auth = {
+  type: "OAuth2",
+  user: "akanuma.9099@gmail.com",
+  clientId: '755195789659-0lt6su9q88eq0585igj83b4m5ont4bbi.apps.googleusercontent.com',
+  clientSecret: "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",
+  refreshToken: "1//04_aWdS9pheLjCgYIARAAGAQSNwF-L9IrHvN4nWm4Th8Q2Bub24PndrddgDhDZZGm3THAbFv22Mt2bRwjxf9eUDjyhvYDNU52pDw",
+};
+
+const transporter = nodemailer.createTransport(transport);
 
 router.post('/', (req, res) => {
   const mailOptions = {
-    from: 'akanuma.9099@gmail.com',
-    to: req.body.email,
+    from: 'from mail address',
+    to:auth.user,
     subject: 'Test Email',
-    text: 'This is a test email.',
+    text: 'メール送信確認テスト',
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
