00ead401c89ee3beab97b32f2c6dd4781410b68d
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-08-12 01:04:17 +0900

    Commit message

--- public/js/MENU/menu.js
+++ public/js/MENU/menu.js
@@ -485,27 +485,31 @@ document
             friendListUpdate();
             document.getElementById('popup-overlay_friend-add').style.display =
               'none';
-            document.getElementById(
-              'popup-overlay_friend-add-ans'
-            ).style.display = 'block';
-            document.getElementById('new-friend-user').innerHTML = res.userName;
+            resultPopUp(
+              'フレンド追加',
+              `"${res.userName}"さんをフレンドリストに追加しました`
+            );
+            // document.getElementById(
+            //   'popup-overlay_friend-add-ans'
+            // ).style.display = 'block';
+            // document.getElementById('new-friend-user').innerHTML = res.userName;
 
-            document
-              .getElementById('popup-overlay_friend-add-ans')
-              .addEventListener('click', (e) => {
-                const popup = document.getElementById(
-                  'popup-overlay_friend-add-ans'
-                );
-                if (e.target === popup) {
-                  popup.style.display = 'none';
-                }
-              });
+            // document
+            //   .getElementById('popup-overlay_friend-add-ans')
+            //   .addEventListener('click', (e) => {
+            //     const popup = document.getElementById(
+            //       'popup-overlay_friend-add-ans'
+            //     );
+            //     if (e.target === popup) {
+            //       popup.style.display = 'none';
+            //     }
+            //   });
 
-            setTimeout(() => {
-              document.getElementById(
-                'popup-overlay_friend-add-ans'
-              ).style.display = 'none';
-            }, 1500);
+            // setTimeout(() => {
+            //   document.getElementById(
+            //     'popup-overlay_friend-add-ans'
+            //   ).style.display = 'none';
+            // }, 1500);
           }
         },
       });
