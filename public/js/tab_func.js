2ca248ee92d3fa80eda77fd64d3167ade0ae532f
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-17 16:46:58 +0900

    Update tab_func.js

--- public/js/tab_func.js
+++ public/js/tab_func.js
@@ -222,7 +222,12 @@ export const cancelButtonClick = async (id) => {
     document.getElementById(`textarea${id}`).readOnly = true;
     document.getElementById(`fade${id}`).style.opacity = '0';
     document.getElementById(`tabname${id}`).style.color = 'black';
-
+    //テキストが「!」で始まっている場合取り除く
+    const element = document.getElementById(`tabname${id}`);
+    if (element.innerHTML.startsWith('!')) {
+     element.innerHTML = element.innerHTML.substring(1);
+    }
+    
     resultPopUp('取り消し', '取り消しました');
   } else {
     // 「いいえ」が押された場合の処理 おそらくポップが閉じる
