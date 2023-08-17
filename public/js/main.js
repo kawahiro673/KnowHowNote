7305a9e979c6764d6a86d387fba3343de02c4ef8
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-17 16:34:08 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -304,6 +304,8 @@ function tabScreenOptions(id, title) {
           tabIdArray.push(id);
 
           document.getElementById(`edit-note-btn${id}`).onclick = function () {
+            const element = document.getElementById(`tabname${id}`);
+　　　　　　　element.innerHTML = `!${element.innerHTML}`;
             document.getElementById(`tabname${id}`).style.color = 'red';
 
             document.getElementById(`fade${id}`).style.opacity = '1';
