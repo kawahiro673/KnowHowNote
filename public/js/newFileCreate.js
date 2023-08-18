d594559cde105b1acc30c1640d17f318afe8406e
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 10:27:09 +0900

    Update newFileCreate.js

--- public/js/newFileCreate.js
+++ public/js/newFileCreate.js
@@ -1,5 +1,5 @@
 import { listCreate } from './main.js';
-import { orderGet, currentTimeGet,explanationPopUp } from './stringUtils.js';
+import { orderGet, currentTimeGet,explanationPopUp, resultPopUp } from './stringUtils.js';
 import { hasInput, disableElements, getInputOrder } from './utilityFunction.js';
 
 export const newFileCreateFunc = (id) => {
@@ -78,7 +78,7 @@ export const newCreateFile2 = (inputTab, parentId, order) => {
   return new Promise((resolve, reject) => {
     //何も入力されていない時や空白や改行のみ
     if (!inputTab.value || !inputTab.value.match(/\S/g)) {
-     //explanationPopUp('名前変更','名前を入力してください');
+     resultPopUp('名前変更','名前を入力してください');
      console.log('yaa');
       reject();
     } else {
