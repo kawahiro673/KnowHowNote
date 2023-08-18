e4cdbbcf21d96d05ae0c1e245ef0789f50615645
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 14:30:07 +0900

    Update mypage.js

--- routes/mypage.js
+++ routes/mypage.js
@@ -360,16 +360,17 @@ router
           console.error(error);
           res.status(500).send('Internal Server Error.(sharelist)');
         });
-
-      //ログアウト時にcookie削除
-    } else if (req.body.flg === 'cookiedelete') {
-      //cookie削除
-      res.setHeader('Set-Cookie', [
-        'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
-        'hashedId=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
-      ]);
-      res.send({ msg: 'ログアウトします' });
-    } else if (req.body.flg === 'ShareList') {
+     } 
+        //ログアウト時にcookie削除
+      //else if (req.body.flg === 'cookiedelete') {
+    //   //cookie削除
+    //   res.setHeader('Set-Cookie', [
+    //     'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
+    //     'hashedId=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
+    //   ]);
+    //   res.send({ msg: 'ログアウトします' });
+    // } 
+      else if (req.body.flg === 'ShareList') {
       getUserDataByToken(req).then((resultDecoded) => {
         return new Promise((resolve, reject) => {
           pool.query(
