455e7fcbdec8c042b18ad0512db849f69f80ecd2
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 15:31:38 +0900

    Update mypage.js

--- routes/mypage.js
+++ routes/mypage.js
@@ -266,58 +266,7 @@ router
           console.error(error);
           res.status(500).send('Internal Server Error.(noteChild)');
         });
-    } else if (req.body.flg === 'deleteALL') {
-      getUserDataByToken(req)
-        .then((resultDecoded) => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'DELETE from folder WHERE UserID = ?',
-              [resultDecoded[0].id],
-              (error, result) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  resolve(resultDecoded);
-                }
-              }
-            );
-          });
-        })
-        .then((resultDecoded) => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'DELETE from it_memo WHERE UserID = ?',
-              [resultDecoded[0].id],
-              (error, result) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  resolve(resultDecoded);
-                }
-              }
-            );
-          });
-        })
-        .then((resultDecoded) => {
-          return new Promise((resolve, reject) => {
-            pool.query(
-              'DELETE from tab_hold WHERE UserID = ?',
-              [resultDecoded[0].id],
-              (error, result) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  res.send({ response: result });
-                }
-              }
-            );
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Internal Server Error.(sharelist)');
-        });
-     }  else if (req.body.flg === 'ShareList') {
+    } else if (req.body.flg === 'ShareList') {
       getUserDataByToken(req).then((resultDecoded) => {
         return new Promise((resolve, reject) => {
           pool.query(
