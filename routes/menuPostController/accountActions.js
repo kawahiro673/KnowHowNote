d9f325a20acf8648a7596a32c403fa67b30722c5
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 14:24:02 +0900

    Update accountActions.js

--- routes/menuPostController/accountActions.js
+++ routes/menuPostController/accountActions.js
@@ -1,1 +1,127 @@
+const { getUserDataByToken } = require('../databaseQueries');
 
+const router = require('express').Router();
+const pool = require('../../db.js');
+const JWT = require('jsonwebtoken');
+const { reject } = require('bcrypt/promises');
+
+router.post('/', (req, res) => {
+ if (req.body.flg === 'Acount-Delte') {
+      getUserDataByToken(req)
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from it_memo where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from folder where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from friend_list where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from group_list where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from tab_hold where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from share_user where UserID = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(resultDecoded);
+                }
+              }
+            );
+          });
+        })
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'DELETE from register_user where id = ?;',
+              [resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  res.setHeader('Set-Cookie', [
+                    'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
+                    'hashedId=; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/',
+                  ]);
+                  res.send({ msg: 'アカウント削除します' });
+                }
+              }
+            );
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ message: error.message, nothing });
+        });
+    }
+});
+
+module.exports = router;
