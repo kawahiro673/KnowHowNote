68a0a9efb6df78ad91e296b354996e76a6c3596f
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 15:37:41 +0900

    Update mypage.js

--- routes/mypage.js
+++ routes/mypage.js
@@ -266,49 +266,6 @@ router
           console.error(error);
           res.status(500).send('Internal Server Error.(noteChild)');
         });
-    } else if (req.body.flg === 'ShareList') {
-      getUserDataByToken(req).then((resultDecoded) => {
-        return new Promise((resolve, reject) => {
-          pool.query(
-            'SELECT id FROM share_user WHERE UserID = ? ORDER BY id DESC LIMIT 100;',
-            [resultDecoded[0].id],
-            (error, rows) => {
-              if (error) {
-                reject(error);
-              } else {
-                // 最新の100行のIDを取得
-                const idsToKeep = rows.map((row) => row.id);
-                if (idsToKeep.length > 0) {
-                  // 最新の100行以外のレコードを削除
-                  pool.query(
-                    'DELETE FROM share_user WHERE UserID = ? AND id NOT IN (?);',
-                    [resultDecoded[0].id, idsToKeep],
-                    (error, deleteResult) => {
-                      if (error) {
-                        reject(error);
-                      } else {
-                        pool.query(
-                          'SELECT * FROM share_user WHERE UserID = ? ORDER BY id DESC;',
-                          [resultDecoded[0].id],
-                          (error, selectResult) => {
-                            if (error) {
-                              reject(error);
-                            } else {
-                              res.send({ shareResult: selectResult });
-                            }
-                          }
-                        );
-                      }
-                    }
-                  );
-                } else {
-                  res.send({ shareResult: null });
-                }
-              }
-            }
-          );
-        });
-      });
     } else if (req.body.flg === 'RegisterUser') {
       const token = req.cookies.token;
       const decoded = JWT.verify(token, 'SECRET_KEY');
