dec9b186ae26eb56de87124d3956d9eb2fc65385
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 14:56:29 +0900

    Update authenticationIDPostController.js

--- routes/menuPostController/authenticationIDPostController.js
+++ routes/menuPostController/authenticationIDPostController.js
@@ -74,6 +74,63 @@ router.post('/', (req, res) => {
           console.error(error);
           res.status(500).json({ message: error.message, nothing });
         });
+    }else if (req.body.flg === 'AuthenticationIDUpdte') {
+      getUserDataByToken(req)
+        .then((resultDecoded) => {
+          return new Promise((resolve, reject) => {
+            let authenticationID = generateRandomID();
+            //かなり確率として低いが、利用者IDがかぶる可能性もあるため、確認している
+            //DBをすべて参照するより重複しているかを確認した方が早いため下記で確認
+            function checkAndInsert() {
+              pool.query(
+                'SELECT COUNT(*) AS count FROM register_user WHERE Authentication_ID = ?',
+                [authenticationID],
+                (error, result) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    const count = result[0].count;
+                    if (count === 0) {
+                      // 重複しない authenticationID が見つかった場合
+                      resolve({
+                        resultDecoded: resultDecoded,
+                        authenticationID: authenticationID,
+                      });
+                    } else {
+                      // 重複する場合は新しい authenticationID を生成して再度チェック
+                      authenticationID = generateRandomID();
+                      checkAndInsert();
+                    }
+                  }
+                }
+              );
+            }
+            // 初回の重複チェックを実行
+            checkAndInsert();
+          });
+        })
+        .then(({ resultDecoded, authenticationID }) => {
+          return new Promise((resolve, reject) => {
+            pool.query(
+              'UPDATE register_user SET Authentication_ID = ? WHERE id = ?;',
+              [authenticationID, resultDecoded[0].id],
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(authenticationID);
+                }
+              }
+            );
+          });
+        })
+        .then((authenticationID) => {
+          res.send({ authenticationID });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ message: error.message, nothing });
+        });
     }
 });
 
