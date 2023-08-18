95017d86d39ca10966bd4484f6567f38ebb7bc86
Kawaguchi Tomohiro <126426280+kawahiro673@users.noreply.github.com>
2023-08-18 16:07:06 +0900

    Update otherMenuPostController.js

--- routes/menuPostController/otherMenuPostController.js
+++ routes/menuPostController/otherMenuPostController.js
@@ -5,6 +5,23 @@ const pool = require('../../db.js');
 const JWT = require('jsonwebtoken');
 const { reject } = require('bcrypt/promises');
 
+//認証情報
+const auth = {
+  type: 'OAuth2',
+  user: 'akanuma.9099@gmail.com',
+  clientId:
+    '755195789659-0lt6su9q88eq0585igj83b4m5ont4bbi.apps.googleusercontent.com',
+  clientSecret: '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',
+  refreshToken:
+    '1//04_aWdS9pheLjCgYIARAAGAQSNwF-L9IrHvN4nWm4Th8Q2Bub24PndrddgDhDZZGm3THAbFv22Mt2bRwjxf9eUDjyhvYDNU52pDw',
+};
+const transport = {
+  service: 'gmail',
+  auth,
+};
+const transporter = nodemailer.createTransport(transport);
+
+
 router.post('/', (req, res) => {
  if (req.body.flg === 'RegisterUser') {
       const token = req.cookies.token;
