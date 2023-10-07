be675b781ff340bea3bbe1eb57910bbc1a8c50cd
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-10-07 09:35:53 +0900

    環境変数

--- routes/check.js
+++ routes/check.js
@@ -1,13 +1,16 @@
 const res = require('express/lib/response');
 const JWT = require('jsonwebtoken');
 
-//Cookieに保存しているトークンが認証できるかどうか確認
+//Cookieに保存しているトークンが
 function check(req, res, next) {
   try {
-    //復号
+    const token = req.cookies.token;
+    //復号する。認証できるかどうか確認
+    const decoded = JWT.verify(token, process.env.Token_KEY);
     const hashedId = req.cookies.hashedId;
     req.value = hashedId;
-    //下記で続きを実行。res.render('index.ejs');
+
+    //下記で続きを実行、今回の場合は res.render('index.ejs');
     next();
   } catch (err) {
     res.render('top.ejs');
